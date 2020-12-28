07ba4f3bd486517ec96b3c0974779597f111371b
noam <noamsi@post.bgu.ac.il>
2020-12-28 19:40:22 +0200

    Update project structure

--- main.py
+++ main.py
@@ -1,31 +1,124 @@
 import numpy as np
 from scipy.io import loadmat
 
+# Initialize
 x = loadmat('NNdata/PeaksData.mat')
 Ct, Cv, Yt, Yv = x['Ct'], x['Cv'], x['Yt'], x['Yv']
 
 
-# Write code for computing the objective loss function “soft-max” and its gradient with
-# respect to wj and the biases. Make sure that the derivatives are correct using the
-# gradient test. You should submit the results of the gradient test.
-
-
-def softmax(W, X, C):
+# Question 1
+def softmax(W, X, C, b):
     """
     l = [num of categories]
     m = [num of samples]
-    d = [num of features per sample]
-    :param W: array of l weight vectors of dimension d = [shape = (d,l)]
-    :param X: array of m samples, of dimension d. [shape = (d,m)]
-    :param C: array of m categorical labels [shape = (l, m)]
+    d = [length of each sample]
+    :param W: array[shape=(d,l)], each column is a weight vector of dimension d
+    :param X: array[shape=(d,m)], each column is a data sample of dimension d
+    :param C: array[shape=(l,m)], each column is a one-hot vector categorical label
+    :param b: array[shape=(
     :return: objective loss function “soft-max”
     """
-    l = len(Ct)
-    m = len(X.T)
+
     d = len(W)
+    m = len(X.T)
+    l = len(C)
     eta = max([np.dot(X.T, W[j]) for j in range(d)])
 
     return -np.sum([C[k].T * np.log(np.exp(np.dot(X.T, W[k]) - eta) / np.exp(X.T @ W - eta)) for k in range(l)]) / m
 
 
 
+def dsoftmax_db():
+    pass
+
+
+def compute_soft_max_gradient():
+    """compute the gradient of the softmax function with respect to Wj and the bias.
+    See question 1.
+    """
+    raise NotImplementedError
+
+
+def test_derivates():
+    """
+    Makes sures the derivatives are correct using the gradient test.
+    See question 1
+    :return: results of the gradient test
+    """
+    raise NotImplementedError
+
+
+
+def gradient_test():
+    """make sure that the derivatives are okay using the gradient test"""
+    raise NotImplementedError
+
+
+# Question 2
+def minimize_objective_function_SGD(func):
+    """
+    Minimizes an objective function.
+    See question 2.
+    :param func:
+    :return: optimal params
+    """
+    raise NotImplementedError
+
+
+# Question 3
+def demonstrate_minimization_of_softmax():
+    """
+    Plots a graph of accuracy of data classification after each epoch.
+    Do the plots for both the training data and the validation data.
+    See question 3.
+    :return:
+    """
+    raise NotImplementedError
+
+
+# Question 4
+def code_for_neural_network():
+    """
+    Includes the forward pass and back-ward pass (the computation of the "Jacobian transpose times vector").
+    See question 4.
+    :return:
+    """
+    raise NotImplementedError
+
+
+def test_jacobians():
+    """
+    See that the Jacobian tests work and submit the tests.
+    See question 4
+    :return:
+    """
+
+
+# Question 6
+def compute_forward_pass_on_network(L):
+    """
+    Compute a forward pass of a network with L layers
+    :param L: num of layers
+    :return:
+    """
+
+# Question 6
+def compute_backward_pass_on_network(L):
+    """
+    Compute a backward pass of a network with L layers
+    :param L: num of layers
+    :return:
+    """
+
+
+def question_6(L):
+    """
+    Compute a forward pass.
+    Compute a backward pass.
+    See the gradient of the whole network passes the gradient test
+    :param L: num of layers
+    :return gradient test
+    """
+    compute_forward_pass_on_network(L)
+    compute_backward_pass_on_network(L)
+    gradient_test()
